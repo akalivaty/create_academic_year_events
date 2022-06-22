f38eb2838d7b7d0092d366e69715a364c9b9c299
akalivaty <73537279+akalivaty@users.noreply.github.com>
2022-06-22 09:49:06 +0800

    hide calendar id

--- remove.js
+++ remove.js
@@ -1,6 +1,6 @@
 function clean_all_events() {
     const thisYear = new Date().getFullYear();
-    const calendar = CalendarApp.getCalendarById('gm.nuu.edu.tw_853lo4f1bcijuhk8h1kkcp6128@group.calendar.google.com');
+    const calendar = CalendarApp.getCalendarById('CALENDAR_ID');
     const events = calendar.getEvents(
         new Date([thisYear, '8/1'].join('/')), 
         new Date([thisYear + 1, '8/1'].join('/')));
