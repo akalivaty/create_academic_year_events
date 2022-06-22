af4f429bf96d9c2d6da7d3a70ad368cf2796c74c
akalivaty <73537279+akalivaty@users.noreply.github.com>
2022-06-22 09:51:26 +0800

    Delete debug.js

--- debug.js
+++
@@ -1,7 +0,0 @@
-function debug() {
-  const ss = SpreadsheetApp.getActiveSheet();
-  const data = ss.getRange(2, 2, ss.getLastRow() - 1, 2).getValues();
-  data.forEach((value, idx) => {
-    console.log(`row: ${idx + 1}, value: ${value[0]}, type: ${typeof (value[0])}, length: ${value[0].split('-').length}`);
-  })
-}
